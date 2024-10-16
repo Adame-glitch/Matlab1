eee95d8f17001cb1dfefc765642bfb141eaacb4b
Adame <76791762+Adame-glitch@users.noreply.github.com>
2024-10-16 19:48:08 +0200

    Update Lab1.m

--- Lab1.m
+++ Lab1.m
@@ -1,29 +1,39 @@
 %% Laboratorium 1
 clc; clear all; close all;
-% wartości nominalne
-TzewN =-20; % temp zewn
-a=2; % Współczynnik przenikania ciepla
-Pgn = 10000; % moc grzałki w [w]
-Tv1 = 20; %temp w 1 pokoju
-Tv2 = 15; %temp w 2gim pokoju
-Vl = 150; % objętość mieszkania, 50x3
-Vp 
-Cp=1000; rop=1.2 % ciepło właściwe powietrza
+% wartosci nominalne
+TzewN = -20; % temp zewn
+a = 2; % Wspolczynnik przenikania ciepla
+B = 5 % Grubosc sciany dzialowej
+Pgn = 10000; % moc grzalki w [w]
+Tl = 20; %temp w 1 pokoju
+Tp = 15; %temp w 2gim pokoju
+x = (50 / B * 2 + 5) / 3
+y = (x - 5) / 2
+Vp = B * x * 3; %Objetosc prawgo pokoju
+Vl = B * y * 3; %Objetosc lewgo pokoju
+Cp = 1000; rop= 1.2 % cieplo wlasciwe powietrza
 
-Ks2=Pgn./(a.*(Tv1-TzewN)+Tv2-TzewN);
-Ks1 = a.*Ks2;
-Ks0 = Ks2.*(Tv2-TzewN)./(Tv1-Tv2);
+Cvl = Cp * rop * Vp;
+Cvp = Cp * rop * Vl;
 
+Ksp = Pgn ./ (a .* (Tl - TzewN) + Tp - TzewN);
+Ksl = a .* Ksp;
+Ksw = Ksp .* (Tp - TzewN) ./ (Tl - Tp); %Przewodnosc temperatury pomiedzy
+%Cvl = (Pgn - Ksl.*(Tl-TzewN) - Ksw.*(Tl-Tp))./Tl; % objetosc cieplna
+%Cvp = (Ksw .*(Tl-Tp)-Ksp.*(Tp-TzewN))./Tp;
 
-Cv1 = (Pgn - Ks1.*(Tv1-TzewN) - Ks0.*(Tv1-Tv2))./Tv1; % objętość cieplna
-Cv2 = (Ks0 .*(Tv1-Tv2)-Ks2.*(Tv2-TzewN))./Tv2;
-Cv1=Cp.*rop.*;
-
-
-%% 2 część pracy
 % Warunki początkowe:
-Tzew0=TzewN+0;
-Pg0=Pgn+0;
-a0=a+0;
+Tzew0 = TzewN+0;
+Pg0 = Pgn+0;
+a0 = a+0;
 
-Tv1=
+% Stan rwnowagi
+Tl0 = (Pg0 + Ksl .* Tzew0 + Ksw .* Tp) ./ (Cvl .*(1 + Ksl + Ksw));
+Tp0 = (Ksw .* Tl + Ksp .* Tzew0) ./ (Cvp .* (1 + Ksw + Ksp));
+
+% Symulacja
+czas = 50000;
+t0 = 0;
+[t] = sim('Lab1_sim.slx', czas);
+figure,plot(t.tout, t.oTl);
+figure,plot(t.tout, t.oTp);
